ccdf9a79a4be9e79182b791728acde40c0d33153
byte4ever <laurent8martin@gmail.com>
2022-06-01 09:42:48 +0200

    improve test coverage

--- utils/hash/hash_test.go
+++ utils/hash/hash_test.go
@@ -17,18 +17,32 @@ func hp(h Hash) *Hash {
 }
 
 func TestHash_MarshalYAML(t *testing.T) {
-	for _, hash := range tab {
-		fori := &FakeStruct{
-			H: hp(Hash(hash)),
-		}
-		bb, err := yaml.Marshal(fori)
-		require.Nil(t, err)
-		require.NotNil(t, bb)
+	t.Run(
+		"all succeed", func(t *testing.T) {
+			for _, hash := range tab {
+				fori := &FakeStruct{
+					H: hp(Hash(hash)),
+				}
+				bb, err := yaml.Marshal(fori)
+				require.Nil(t, err)
+				require.NotNil(t, bb)
 
-		var fs FakeStruct
-		err = yaml.Unmarshal(bb, &fs)
-		require.Nil(t, err)
-		require.Equal(t, fori.H.ToCrypto(), fs.H.ToCrypto())
-		require.True(t, fs.H.ToCrypto().Available())
-	}
+				var fs FakeStruct
+				err = yaml.Unmarshal(bb, &fs)
+				require.Nil(t, err)
+				require.Equal(t, fori.H.ToCrypto(), fs.H.ToCrypto())
+				require.True(t, fs.H.ToCrypto().Available())
+			}
+		},
+	)
+
+	t.Run(
+		"unmarshal test failure", func(t *testing.T) {
+			var h Hash
+			err := h.UnmarshalText([]byte("INVALID_HASH"))
+			require.ErrorIs(t, err, ErrInvalidHash)
+			require.ErrorContains(t, err, "INVALID_HASH")
+		},
+	)
+
 }
