2c7043d037bef4c1916a7917ce8e82871cea4313
byte4ever <laurent8martin@gmail.com>
2022-06-06 18:34:13 +0200

    examples for cmdline provider

---
+++ providers/sbased/cmdline/examples_test.go
@@ -0,0 +1,66 @@
+package cmdline
+
+import (
+	"fmt"
+	"sort"
+)
+
+func Example_newEntriesProvider_1() {
+	// defines params line command. os.Arg[1:] is commonly used.
+	params := []string{"some-thing"}
+
+	_, err := NewEntriesProvider(params)
+
+	fmt.Println("when processing invalid params got error:")
+	fmt.Println(err)
+	// Output:
+	// when getting invalid params got error:
+	// arg #0 - (some-thing): options param not in --xxx=val format
+}
+
+func Example_newEntriesProvider_2() {
+	// defines params line command. os.Arg[1:] is commonly used.
+	params := []string{"--arg1=1", "--arg2=1", "--arg1=asdasd"}
+
+	_, err := NewEntriesProvider(params)
+
+	fmt.Println("when processing duplicated params got error:")
+	fmt.Println(err)
+	// Output:
+	// when processing duplicated params got error:
+	// --arg1: duplicate param
+}
+
+func Example_newEntriesProvider_3() {
+	// defines params line command. os.Arg[1:] is commonly used.
+	params := []string{"--arg3=1", "--arg1=10", "--arg2=asdasd"}
+
+	provider, _ := NewEntriesProvider(params)
+
+	fmt.Println("when processing params got those entries:")
+
+	entries := provider.GetEntries()
+	sortedKeys := make([]string, 0, len(entries))
+
+	for key := range entries {
+		sortedKeys = append(sortedKeys, key)
+	}
+
+	sort.Strings(sortedKeys)
+
+	for i, key := range sortedKeys {
+		entry := entries[key]
+		fmt.Printf(
+			"%2d - key=%q externalKey=%q value=%q\n",
+			i,
+			key,
+			entry.ExternalKey,
+			entry.Value,
+		)
+	}
+	// Output:
+	// when processing params got those entries:
+	//  0 - key="arg1" externalKey="--arg1" value="10"
+	//  1 - key="arg2" externalKey="--arg2" value="asdasd"
+	//  2 - key="arg3" externalKey="--arg3" value="1"
+}
