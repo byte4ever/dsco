407fe421d4c5dbe90ade0fcd6ef060685437f8b7
byte4ever <laurent8martin@gmail.com>
2026-04-24 14:46:01 +0200

    inventory: scaffold package and core data types

---
+++ inventory/doc.go
@@ -0,0 +1,6 @@
+// Package inventory computes the static list of configuration keys a
+// dsco-managed struct expects, given a set of layers, without performing
+// any I/O.
+//
+// See https://github.com/byte4ever/dsco for the parent project.
+package inventory
---
+++ inventory/inventory.go
@@ -0,0 +1,30 @@
+package inventory
+
+// Report is the static inventory of a config struct against a layer set.
+type Report struct {
+	Type   string  `json:"type"   yaml:"type"`
+	Fields []Field `json:"fields" yaml:"fields"`
+}
+
+// Field describes the canonical key (and any baked-in default) for one
+// leaf field of the config struct.
+type Field struct {
+	Path      string        `json:"path"                yaml:"path"`
+	GoType    string        `json:"go_type"             yaml:"go_type"`
+	Satisfied *Satisfaction `json:"satisfied,omitempty" yaml:"satisfied,omitempty"`
+	Key       *KeySpec      `json:"key,omitempty"       yaml:"key,omitempty"`
+}
+
+// Satisfaction records that a struct layer already provides a value
+// for this field.
+type Satisfaction struct {
+	LayerID string `json:"layer_id" yaml:"layer_id"`
+	Value   any    `json:"value"    yaml:"value"`
+}
+
+// KeySpec is the canonical (highest-precedence) key form a string-based
+// layer would accept to supply this field.
+type KeySpec struct {
+	Layer string `json:"layer" yaml:"layer"`
+	Key   string `json:"key"   yaml:"key"`
+}
